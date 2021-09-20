6a63d8676cb3f2927ad57e9d66ca9601fff15ea5
zhichengzhao <60787822+zhichengzhao@users.noreply.github.com>
2021-09-20 16:01:06 -0400

    map

--- app/src/main/java/edu/temple/foodiego/LoginActivity.java
+++ app/src/main/java/edu/temple/foodiego/LoginActivity.java
@@ -3,12 +3,33 @@ package edu.temple.foodiego;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editTextUserName,
+            editTextPassword,
+            editTextFirstName,
+            editTextLastName;
+    private Button
+            buttonOK,
+            buttonRegisterOrCancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextUserName = findViewById(R.id.etLogInUserName);
+        editTextPassword = findViewById(R.id.etLogInPassword);
+        editTextFirstName = findViewById(R.id.etLogInFName);
+        editTextLastName = findViewById(R.id.etLogInLName);
+        buttonOK = findViewById(R.id.btnOK);
+        buttonRegisterOrCancel = findViewById(R.id.btnRegOrCan);
+
+        buttonOK.setOnClickListener(v -> {
+            finish();
+        });
+
     }
 }
--- app/src/main/java/edu/temple/foodiego/MainActivity.java
+++ app/src/main/java/edu/temple/foodiego/MainActivity.java
@@ -1,14 +1,41 @@
 package edu.temple.foodiego;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    Intent[] intents = new Intent[10];
+    GoogleMap  googleMap;
+    MapView mapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intents[0] = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intents[0]);
+
+        //Map
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync( this);
+    }
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        //googleMap1.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(0,0), 18.0f));
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 }
