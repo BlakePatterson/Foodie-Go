1593b79b043f5d19e50ff9ee4c7805b0ecbd9174
Blake Patterson <brpatterson00@gmail.com>
2021-10-22 16:57:57 -0400

    Created map fragment & loaded it into frame layout in map activity

--- app/src/main/java/edu/temple/foodiego/MapActivity.java
+++ app/src/main/java/edu/temple/foodiego/MapActivity.java
@@ -3,7 +3,9 @@ package edu.temple.foodiego;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -13,7 +15,11 @@ import static android.content.ContentValues.TAG;
 
 public class MapActivity extends AppCompatActivity {
 
+    static int permissionRequestCode = 12345;
+
     public FoodieUser user;
+
+    private MapFragment mapFragment;
 
     private SharedPreferences preferences;
 
@@ -31,6 +37,13 @@ public class MapActivity extends AppCompatActivity {
             String lastname = startIntent.getString(getString(R.string.lastname_bundle_key));
             Log.d(TAG, "onCreate: MapActivity launched with username: " + username + "; firstname: " + firstname + "; lastname: " + lastname);
             user = new FoodieUser(username, firstname, lastname);
+        }
+
+        // Check to see if location permissions have been granted before loading any of the actual content
+        if (!hasGPSPermission()) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, permissionRequestCode);
+        } else {
+            loadFragments();
         }
 
     }
@@ -64,6 +77,31 @@ public class MapActivity extends AppCompatActivity {
 
         //return to the login activity
         finish();
+    }
+
+    public boolean hasGPSPermission() {
+        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == permissionRequestCode && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            loadFragments();
+        }
+    }
+
+    private void loadFragments() {
+        if (!(getSupportFragmentManager().findFragmentById(R.id.mapFrameLayout) instanceof MapFragment)) {
+            mapFragment = MapFragment.newInstance(user.getUsername(), user.getFirstname(), user.getLastname());
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.mapFrameLayout, mapFragment)
+                    .commit();
+        }
+        else {
+            mapFragment = (MapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFrameLayout);
+        }
     }
 
 }
---
+++ app/src/main/java/edu/temple/foodiego/MapFragment.java
@@ -0,0 +1,100 @@
+package edu.temple.foodiego;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment {
+
+    public static final String USERNAME_PARAM_KEY = "mapParam1";
+    public static final String FIRSTNAME_PARAM_KEY = "mapParam2";
+    public static final String LASTNAME_PARAM_KEY = "mapParam3";
+
+    private GoogleMap map;
+    private Marker userMarker;
+
+    private Context parentActivity;
+
+    private FoodieUser user;
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng sydney = new LatLng(-34, 151);
+            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+    };
+
+    public static MapFragment newInstance(String username, String firstname, String lastname) {
+        Bundle args = new Bundle();
+        args.putSerializable(USERNAME_PARAM_KEY, username);
+        args.putSerializable(FIRSTNAME_PARAM_KEY, firstname);
+        args.putSerializable(LASTNAME_PARAM_KEY, lastname);
+        MapFragment fragment = new MapFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() == null){
+            user = new FoodieUser("", "", "");
+        } else {
+            String username = (String) getArguments().getSerializable(USERNAME_PARAM_KEY);
+            String firstname = (String) getArguments().getSerializable(FIRSTNAME_PARAM_KEY);
+            String lastname = (String) getArguments().getSerializable(LASTNAME_PARAM_KEY);
+
+            user = new FoodieUser(username, firstname, lastname);
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        parentActivity = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
