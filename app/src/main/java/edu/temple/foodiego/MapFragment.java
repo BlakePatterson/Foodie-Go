92e5b9812498f06ce8a5c0bc191280a8b4aa212e
zhichengzhao <60787822+zhichengzhao@users.noreply.github.com>
2021-10-25 15:38:32 -0400

    value event listener for updating the marker of location, and make add near by locations invisible when distance is too far

--- app/src/main/java/edu/temple/foodiego/MapFragment.java
+++ app/src/main/java/edu/temple/foodiego/MapFragment.java
@@ -20,12 +20,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MapFragment extends Fragment {
 
@@ -41,11 +48,52 @@ public class MapFragment extends Fragment {
     private FoodieUser user;
 
     //foodieLocations and marker onclick behavior.
-    private final String NameString = "username";
+    private final String NameString = "name";
     private final String LatString = "latitude";
     private final String LngString = "longitude";
-    private final String RatingString = "longitude";
+    private final String RatingString = "rating";
+    private final double DISTANCE = 320;
     private ArrayList<FoodieLocation> foodieLocations;
+
+    FirebaseDatabase fbdb = FirebaseDatabase.getInstance();
+    DatabaseReference locationRef = fbdb.getReference("location");
+    private ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+            for (FoodieLocation f: foodieLocations) {
+                f.getMarker().remove();
+            }
+            foodieLocations.clear();
+            String data = String.valueOf(snapshot.getValue());
+            Log.e("data on change", data);
+            try {
+                JSONObject jodata = new JSONObject(data);
+                Iterator<String> keys = jodata.keys();
+                while (keys.hasNext())
+                {
+                    String key = keys.next();
+                    JSONObject jo = (JSONObject) jodata.get(key);
+
+                    //convert the json obj into actual data.
+                    String locationName = jo.getString(NameString);
+                    double locationLat = Double.parseDouble(jo.getString(LatString));
+                    double locationLng = Double.parseDouble(jo.getString(LngString));
+                    double locationRating = Double.parseDouble(jo.getString(RatingString));
+
+                    FoodieLocation foodieLocation = new FoodieLocation(locationName, locationLat,locationLng,locationRating);
+                    foodieLocation.setMarker(map.addMarker(new MarkerOptions().position(new LatLng(locationLat,locationLng)).title(locationName)));
+                    foodieLocations.add(foodieLocation);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
 
 
 
@@ -63,7 +111,18 @@ public class MapFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             map = googleMap;
-            map.setOnMarkerClickListener(onClickLocation());
+            map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(@NonNull Marker marker) {
+                    onClickLocation(marker.getTitle());
+                    return false;
+                }
+            });
+            if(foodieLocations == null)
+            {
+                foodieLocations = new ArrayList<>();
+            }
+            locationRef.addValueEventListener(valueEventListener);
         }
     };
 
@@ -128,6 +187,7 @@ public class MapFragment extends Fragment {
                 userMarker = map.addMarker(new MarkerOptions().position(locationCoords).title(user.getFirstname() + " " + user.getLastname()));
 
             map.animateCamera(CameraUpdateFactory.newLatLngZoom(locationCoords, 17.0f));
+            addNearByLocationsToMap();
         }
     }
 
@@ -135,81 +195,29 @@ public class MapFragment extends Fragment {
         void openLocationDetailView(FoodieLocation location);
     }
 
-    //@param JsonArray
-    //This method first copy what data we have into a new Array list-data not update,
-    //Then it loop through the data see if the data exists, if exist remove the data from data not update,
-    //if not add a new foodielication to foodielocations.
-    //and finally remove the marker that is not updated.
-    //Test: when new data come in, new marker shows up.
-    //      when data that is not update, foodielocation removed and marker disappear.
-    //Result: pass. oct 24 2021 12:10 am.
-
     // addNearbyLocationsToMap
-    public void updateFoodieLocations(JSONArray jsonArray)  {
-        if(foodieLocations == null)
-        {
-            foodieLocations = new ArrayList<>();
-        }
-        //get the full list of user's markers.
-        ArrayList dataNotUpdated = new ArrayList();
-        if(foodieLocations.size()>0)
-        {
-            for (Object s:foodieLocations.toArray()) {
-                dataNotUpdated.add(s);
-            }
-        }
+    // @No param
+    // get the location of user then calculate the distance to the foodielocation.
+    // If distance is less than the range
+    // make it visible
+    // else make it invisible
+    public void addNearByLocationsToMap() {
+        //load user location
+        //TODO: replace the fake user data
+        Location userLocation = new Location("user");
+        userLocation.setLongitude(userMarker.getPosition().longitude);
+        userLocation.setLatitude(userMarker.getPosition().latitude);
 
-        //loop through the data that received
-        for (int i =0; i< jsonArray.length(); i++)
-        {
-            JSONObject jo = null;
-            try {
-                jo = (JSONObject) jsonArray.get(i);
-
-            //convert the json obj into actual data.
-            String locationName = jo.getString(NameString);
-            double locationLat = Double.parseDouble(jo.getString(LatString));
-            double locationLng = Double.parseDouble(jo.getString(LngString));
-            double locationRating = Double.parseDouble(jo.getString(RatingString));
-            //LatLng latLng = new LatLng(lat,lng);
-            FoodieLocation foodieLocation = new FoodieLocation(locationName, locationLat,locationLng,locationRating);
-            //add a new marker if the location previously not exist.
-
-            if( !foodieLocations.contains(foodieLocation))
-            {
-                MarkerOptions mo = new MarkerOptions();
-                mo.title(foodieLocation.getName());
-
-                // TODO: 10/21/2021
-                //mo.snippet("put extra informatio here.");
-                LatLng latLng = new LatLng(foodieLocation.getLocation().getLatitude(),foodieLocation.getLocation().getLongitude());
-                Marker m = map.addMarker(new MarkerOptions().position(latLng));
-                m.setTitle(locationName);
-                m.showInfoWindow();
-                foodieLocation.setMarker(m);
-                foodieLocations.add(foodieLocation);
-                //foodieLocations.put(key,m);
+        for (FoodieLocation f : foodieLocations) {
+            if (f.getLocation().distanceTo(userLocation) <= DISTANCE) {
+                //Log.e("distance", String.valueOf(f.getLocation().distanceTo(userLocation)));
+                f.getMarker().setVisible(true);
+            } else {
+                f.getMarker().setVisible(false);
             }
-            else
-            {
-                dataNotUpdated.remove(foodieLocation);
-            }
-            } catch (JSONException e) {
-                e.printStackTrace();
-                Log.e("Update foodie locations",e.getMessage());
-            }
-
-        }
-        //remove the marker if the location is not received from server.
-        if(dataNotUpdated.size()>0)
-        {
-            for (Object s: dataNotUpdated) {
-                ((FoodieLocation)s).getMarker().remove();
-                foodieLocations.remove(((FoodieLocation)s));
-            }
-            dataNotUpdated = null;
         }
     }
+
 
     //Marker onclick behavior, tell map activity to handle it.
     //TODO: Replace the s string inorder to show data of a location.
@@ -217,22 +225,14 @@ public class MapFragment extends Fragment {
     //Progress: havent done.
     //Test: when user click on a marker which belongs to a foodieLocation, a new activity starts and shows the detail of that foodieLocation.
     //Result: haven't test yet.
-    public GoogleMap.OnMarkerClickListener onClickLocation() {
-        return new GoogleMap.OnMarkerClickListener() {
-            @Override
-            public boolean onMarkerClick(@NonNull Marker marker) {
-                String s = "";
-                Intent intent = new Intent(getActivity(), LocationDetailActivity.class);
-                intent.putExtra("data",s);
-                startActivity(intent);
-                return false;
-            }
-        };
+    public void onClickLocation(String title) {
+        Intent intent = new Intent(getActivity(), LocationDetailActivity.class);
+        intent.putExtra("data",title);
+        startActivity(intent);
     }
 
     public void startRouteToLocation()
     {
-
     }
 
 
