48e34645c38ac0ee0aa28d0a9f5e62bc4d570094
Alan <mathwiz2008@gmail.com>
2021-11-15 13:12:37 -0500

    added leave review dialog

--- app/src/main/java/edu/temple/foodiego/LocationDetailActivity.java
+++ app/src/main/java/edu/temple/foodiego/LocationDetailActivity.java
@@ -5,11 +5,18 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +35,7 @@ import java.util.Iterator;
 public class LocationDetailActivity extends AppCompatActivity {
     final String location_database = "location";
     TextView foodieName;
+    Button reviewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +47,38 @@ public class LocationDetailActivity extends AppCompatActivity {
 
         foodieName = findViewById(R.id.TVLocationName);
         foodieName.setText(s);
+        reviewButton = findViewById(R.id.launchReviewButton);
+        reviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLeaveReviewDialog();
+            }
+        });
+    }
+    public void openLeaveReviewDialog(){
+        new AlertDialog.Builder(this).setView(R.layout.dialog_leave_review)
+                .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Dialog d = (Dialog) dialog;
+                        RatingBar bar = d.findViewById(R.id.ratingBar);
+                        EditText reviewBox = d.findViewById(R.id.reviewBox);
+                        double rating = bar.getRating();
+                        String review = reviewBox.getText().toString();
+                        if(review.equals("") || rating == 0){
+                            Toast.makeText(LocationDetailActivity.this, "Please fill in both the rating and the review.", Toast.LENGTH_LONG).show();
+                        }else{
+                            //call postReview()
+                            d.dismiss();
+                        }
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
