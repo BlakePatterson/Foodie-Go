ebda6bdc6796471249e6eab194d9de11b8ee4ffd
Alan <mathwiz2008@gmail.com>
2021-11-16 10:40:07 -0500

    fixed missing import and missing close brace

--- app/src/main/java/edu/temple/foodiego/FirebaseHelper.java
+++ app/src/main/java/edu/temple/foodiego/FirebaseHelper.java
@@ -132,7 +132,7 @@ public class FirebaseHelper {
         RequestQueue queue = Volley.newRequestQueue(context);
         queue.add(sq);
     }
-    public static void postReview(FoodieUser user, FoodieLocation location, double rating, String review){
+    public static void postReview(FoodieUser user, FoodieLocation location, double rating, String review) {
         //TODO: test that this works properly
         //get reference to reviews
         DatabaseReference reviewsRef = instance.database.getReference("location_review");
@@ -142,7 +142,7 @@ public class FirebaseHelper {
         reviewsRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if(task.isSuccessful()){
+                if (task.isSuccessful()) {
                     //make map with review data
                     HashMap<String, String> map = new HashMap<>();
                     map.put("user", user.getKey());
@@ -151,12 +151,13 @@ public class FirebaseHelper {
                     map.put("review", review);
                     //put the data into the database
                     reviewsRef.child(key).setValue(map);
-                }else{
+                } else {
                     Log.d(TAG, "onComplete: error contacting server");
                     Toast.makeText(ctxt, "Error contacting database. Please try again later.", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
     public static void openAddFriendDialog(Context c, FoodieUser user){
         new AlertDialog.Builder(c).setView(R.layout.dialog_add_friend)
                 .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
--- app/src/main/java/edu/temple/foodiego/LocationDetailActivity.java
+++ app/src/main/java/edu/temple/foodiego/LocationDetailActivity.java
@@ -21,6 +21,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
