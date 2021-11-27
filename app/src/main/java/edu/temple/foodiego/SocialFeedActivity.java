d6935876de8807f82dabf23be4b3036ec40df517
Blake Patterson <brpatterson00@gmail.com>
2021-11-26 19:06:42 -0500

    added menu option to open social feed activity

--- app/src/main/java/edu/temple/foodiego/SocialFeedActivity.java
+++ app/src/main/java/edu/temple/foodiego/SocialFeedActivity.java
@@ -3,9 +3,13 @@ package edu.temple.foodiego;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 public class SocialFeedActivity extends AppCompatActivity {
     private FoodieUser user;
@@ -15,20 +19,38 @@ public class SocialFeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_feed);
+
+        Bundle startIntent = getIntent().getExtras();
+        if(startIntent != null) {
+            String username = startIntent.getString(getString(R.string.username_bundle_key));
+            String firstname = startIntent.getString(getString(R.string.firstname_bundle_key));
+            String lastname = startIntent.getString(getString(R.string.lastname_bundle_key));
+            String key = startIntent.getString(getString(R.string.key_bundle_key));
+            user = new FoodieUser(username, firstname, lastname, key);
+
+            Log.d(TAG, "onCreate: social feed activity started with: user: " + user.getUsername());
+        } else {
+            user = new FoodieUser("", "", "", "");
+        }
+
         list = findViewById(R.id.socialFeed);
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         list.setAdapter(new SocialFeedAdapter(getFriendsActivity()));
     }
+
     public ArrayList<FoodieActivityLog> getFriendsActivity(){
         //TODO: get data from FirebaseHelper
         return new ArrayList<>();
     }
+
     public void openPostActivityDialog(){
 
     }
+
     public void postActivityToFirebase(FoodieActivityLog activity){
 
     }
